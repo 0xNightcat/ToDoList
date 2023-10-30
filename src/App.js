3f8ce308f34d5d5a4f21b07fdca4a000f1653d77
Behnam <nightcat.sec@gmail.com>
2023-10-30 18:51:08 +0330

    structure updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Container, Row, Col, Card, Button, Form } from 'react-bootstrap';
 
+// App component
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Container className='text-center'>
+        <h1 className=''>ToDoList</h1>
+
+        <div className='input-area'>
+          <Form>
+            <Form.Group>
+              <Form.Control type='text' placeholder='New Todo' />           
+            </Form.Group>
+          </Form>
+        </div>
+      </Container>
     </div>
-  );
+  )
 }
 
 export default App;
